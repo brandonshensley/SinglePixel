9d522e2639f5d9df178a431f9a1ae67df0d00d29
Phil Bull <philbull@gmail.com>
2017-03-07 22:29:01 -0800

    * Allow frequency-dependent noise curves to be specified  * Use more realistic CMB/FG input amplitudes, in T and P

--- run_mcmc.py
+++ run_mcmc.py
@@ -1,42 +1,54 @@
 #!/usr/bin/python
-
+"""
+Do MCMC runs to fit FG models to simulated data, over a grid of 
+(nu_min, nu_max) values.
+"""
 import numpy as np
-import matplotlib
-#matplotlib.use('Agg') # Need this to avoid using X server
-import pylab as plt
-import single_pixel as fg
 import models
 import fitting
+import sys
 from multiprocessing import Pool
+
+# Reference noise curve
+NOISE_FILE = "core_plus_extended_noise.dat"
 
 # Band parameter definitions
 nbands = 7
 NPROC = 4 #32
+
 SEED = 10
+if len(sys.argv) > 1:
+    SEED = int(sys.argv[1])
+print "SEED =", SEED
 
 np.random.seed(SEED)
 
+numin_vals = [15., 20., 25., 30., 35., 40.]
+numax_vals = [300., 400., 500., 600., 700., 800.]
 #numin_vals = [5., 10., 20., 30., 40., 50., 60., 70.]
 #numax_vals = [200., 300., 400., 500., 600., 700.]
-numin_vals = [5., ] #10., 20., 30., 40., 50., 60., 70.]
-numax_vals = [100.,] # 300., 400., 500., 600., 700.]
+#numin_vals = [5., ] #10., 20., 30., 40., 50., 60., 70.]
+#numax_vals = [100.,] # 300., 400., 500., 600., 700.]
 
 # Temperature/polarisation noise rms for all bands, as a fraction of T_cmb
-fsigma_T = 0.01
-fsigma_P = 0.01
+fsigma_T = 1. #0.01
+fsigma_P = 2. #0.01
 
 # Define input models and their amplitudes/parameters
-dust_model = models.DustMBB(amp_I=10., amp_Q=1., amp_U=1.2, dust_beta=1.6, dust_T=20.)
-sync_model = models.SyncPow(amp_I=10., amp_Q=2., amp_U=1.5, sync_beta=-3.)
-cmb_model = models.CMB(amp_I=100., amp_Q=10., amp_U=20.)
+#dust_model = models.DustMBB(amp_I=150., amp_Q=10., amp_U=10., dust_beta=1.6, dust_T=20.)
+dust_model = models.DustSimpleMBB(amp_I=150., amp_Q=10., amp_U=10., dust_beta=1.6, dust_T=20.)
+sync_model = models.SyncPow(amp_I=30., amp_Q=10., amp_U=10., sync_beta=-3.2)
+cmb_model = models.CMB(amp_I=50., amp_Q=0.6, amp_U=0.6)
 
-name_in = "MBBSync"
+#name_in = "MBBSync"
+name_in = "SimpleMBBSync"
 mods_in = [dust_model, sync_model, cmb_model]
 amps_in = np.array([m.amps() for m in mods_in])
 params_in = np.array([m.params() for m in mods_in])
 
 # Define models to use for the fitting
-name_fit = "MBBSync"
+#name_fit = "MBBSync"
+name_fit = "SimpleMBBSync"
 mods_fit = [dust_model, sync_model, cmb_model]
 
 
@@ -47,39 +59,6 @@ def bands_log(nu_min, nu_max, nbands):
     """
     freq_vec = np.arange(nbands)
     return nu_min * (nu_max/nu_min)**(freq_vec/(nbands-1.)) * 1e9 # in Hz
-
-def bands_3groups(nu_low, nu_mid, nu_high, sep_low, sep_mid, sep_high, nbands):
-    """
-    Divide bands between three groups: low-, mid-, and high-frequency. Each 
-    group is specified by a minimum frequency. Spacings are linear within each 
-    group.
-    """
-    # Divide available bands into 3 groups
-    nlow = nbands // 3
-    nmid = nbands // 3
-    nhigh = nbands // 3
-    
-    # Assign remainder bands into mid- and high-freq. groups respectively
-    nleft = nbands - (nlow + nmid + nhigh)
-    if nleft == 2:
-        nmid += 1
-        nhigh += 1
-    elif nleft == 1:
-        nmid += 1
-    else:
-        pass
-    
-    # Define bands
-    nu = np.concatenate( [nu_low + sep_low * np.arange(nlow),
-                          nu_mid + sep_mid * np.arange(nmid),
-                          nu_high + sep_high * np.arange(nhigh),] )
-    return nu * 1e9 # in Hz
-
-
-# FIXME: Testing
-#nu = np.array([10., 70., 140., 200.])
-#D_vec, Ninv = fitting.generate_data(nu, fsigma_T, fsigma_P, components=mods)
-#fitting.model_test(nu, D_vec, Ninv, mods)
 
 
 # Expand into all combinations of nu_min,max
@@ -105,11 +84,12 @@ def run_model(nu_params):
     
     # Simulate data and run MCMC fit
     D_vec, Ninv = fitting.generate_data(nu, fsigma_T, fsigma_P, 
-                                        components=mods_in)
+                                        components=mods_in, 
+                                        noise_file=NOISE_FILE)
                                         
     gls_cmb, cmb_chisq, cmb_noise \
             = fitting.model_test(nu, D_vec, Ninv, mods_fit, 
-                                 burn=200, steps=1000,
+                                 burn=200, steps=800,
                                  cmb_amp_in=cmb_model.amps(),
                                  sample_file=fname_samples)
     
